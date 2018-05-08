1e6b05e46fb335a7f66dd80e75e3bdd45c994538
folkert89 <folkertburghout@hotmail.com>
2018-05-08 23:39:01 +0200

    linking the pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root 'events#index'
   get '/events' => 'events#index'
-get '/events/:id' => 'events#show'
+get '/events/:id' => 'events#show', as: "event"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
